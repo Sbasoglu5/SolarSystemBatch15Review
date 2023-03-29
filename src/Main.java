41cd1343614ee5b5e6f47a9856c1eb370ba0cb36
Sbasoglu5 <sezarbasoglu@gmail.com>
2023-03-29 15:02:16 -0400

    the basic of solar system is ready and deployed

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        SolarSystem solar=new SolarSystem();
+        solar.planet="2";
+        solar.moon="2";
+        solar.sun="1";
+    }
+}
